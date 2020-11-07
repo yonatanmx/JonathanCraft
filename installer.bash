817c72c24aae527792f59362ee5bc91a6a659f41
Jonathan Marx <marxyonatan@gmail.com>
2020-11-07 16:24:02 +0200

    Add files via upload

---
+++ installer.bash
@@ -0,0 +1,36 @@
+	cd ~
+	sudo apt install default-jdk
+	mkdir JonathanCraftV6
+	cd JonathanCraftV6
+	wget "https://raw.githubusercontent.com/yonatanmx/JonathanCraft/main/run.jar"
+	wget "https://raw.githubusercontent.com/yonatanmx/JonathanCraft/main/a.bash"
+	chmod +x a.bash
+	mkdir res
+	cd res
+	mkdir worlds
+	cd worlds
+	wget "https://raw.githubusercontent.com/yonatanmx/JonathanCraft/main/res/worlds/level1.txt"
+	wget "https://raw.githubusercontent.com/yonatanmx/JonathanCraft/main/res/worlds/level2.txt"
+	wget "https://raw.githubusercontent.com/yonatanmx/JonathanCraft/main/res/worlds/level3.txt"
+	wget "https://raw.githubusercontent.com/yonatanmx/JonathanCraft/main/res/worlds/level4.txt"
+	wget "https://raw.githubusercontent.com/yonatanmx/JonathanCraft/main/res/worlds/level5.txt"
+	wget "https://raw.githubusercontent.com/yonatanmx/JonathanCraft/main/res/worlds/level6.txt"
+	wget "https://raw.githubusercontent.com/yonatanmx/JonathanCraft/main/res/worlds/level7.txt"
+	wget "https://raw.githubusercontent.com/yonatanmx/JonathanCraft/main/res/worlds/level8.txt"
+	wget "https://raw.githubusercontent.com/yonatanmx/JonathanCraft/main/res/worlds/level9.txt"
+	wget "https://raw.githubusercontent.com/yonatanmx/JonathanCraft/main/res/worlds/level10.txt"
+	wget "https://raw.githubusercontent.com/yonatanmx/JonathanCraft/main/res/worlds/world1.txt"
+	cd -
+	mkdir textures
+	cd textures
+	wget "https://github.com/yonatanmx/JonathanCraft/raw/main/res/textures/inventoryScreen.png"
+	wget "https://github.com/yonatanmx/JonathanCraft/raw/main/res/textures/menu.png"
+	wget "https://github.com/yonatanmx/JonathanCraft/raw/main/res/textures/sheet.png"
+	cd -
+	mkdir fonts
+	cd fonts
+	wget "https://github.com/yonatanmx/JonathanCraft/raw/main/res/fonts/Anton-Regular.ttf"
+	wget "https://github.com/yonatanmx/JonathanCraft/raw/main/res/fonts/slkscr.ttf"
+	cd ~
+	cd Desktop
+	wget "https://github.com/yonatanmx/JonathanCraft/raw/main/JonathanCraft Launcher.desktop"
